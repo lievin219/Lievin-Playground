172ee79369f5d5c22838701537d74f469e78227f
Gakiza Lievin <Gakiza@192.168.1.71>
2026-06-10 12:19:04 +0200

    TS: Types

--- 01 Basics.ts
+++ 01 Basics.ts
@@ -11,3 +11,11 @@ interface User{
  type combination=Number|string
 
   const gakiza:combination=3000
+
+   function getLength(characters:number[]|string){
+       if(typeof characters==='string'){
+          console.log(characters.length)
+       }
+       console.log(characters.length)
+   }
+   getLength
