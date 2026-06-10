f493ffbd0fad84df90de21a93ebb2830ee11cd85
Gakiza Lievin <Gakiza@192.168.1.71>
2026-06-10 11:25:01 +0200

    js:ideal classes capabilities

---
+++ 01 Basics.ts
@@ -0,0 +1,9 @@
+interface User{
+     name:string;
+     id:number
+}
+
+ const users:User={
+    name:'Gakiza Lievin',
+    id:80
+ }
---
+++ classes.js
@@ -0,0 +1,10 @@
+class perimeter{
+    constructor(height,width){
+         this.height=height;
+         this.square=width
+    }
+}
+
+const exampe=new perimeter(22,33)
+
+console.log(exampe.square)
